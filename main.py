be9fea7ce744ee9bfe3edfe725154f897f200b44
Reaxo <110986239+imReaxo@users.noreply.github.com>
2022-11-28 19:47:27 +0330

    Update main.py

--- main.py
+++ main.py
@@ -25,18 +25,9 @@ import time
 
 
 #---------------CONFIG--------------
-with open('data/app.json') as f:
-            config = json.load(f)
-            if config.get('TOKEN') == "your_token_here":
-                if not os.environ.get('TOKEN'):
-                    self.run_wizard()
-            else:
-                token = config.get('TOKEN').strip('\"')
-        return os.environ.get('TOKEN') or token
-
 PREFIX = "+"
 NAME = "Tr7hard"
-TOKEN = token
+TOKEN = "S3CR3T"
 ICON = "https://cdn.discordapp.com/avatars/993170609230073896/42779ca341225056ef6d69bffefaaf84.png?size=1024"
 #---------------CONFIG--------------
 
